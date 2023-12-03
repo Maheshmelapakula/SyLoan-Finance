6eb378062e98deebb45201109d812732005e7369
AnandAK07 <anandkumark055@gmail.com>
2023-12-04 02:40:27 +0530

    msg & mail sending to firebase storage

--- syloan_finance/src/Pages/ContactUs.jsx
+++ syloan_finance/src/Pages/ContactUs.jsx
@@ -1,19 +1,54 @@
 import React, { useState } from 'react'
 import style from './Contact.module.css'
 import Footer from '../Components/Footer'
+import { app,database } from '../firebaseConfig';
+import {collection,addDoc,getDocs,doc,updateDoc,deleteDoc,onSnapshot} from 'firebase/firestore';
+  
 
 export const ContactUs = () => {
-  const [email,setEmail]=useState({
+    const [mail,setMail]=useState({})
+    const [msg,setMsg]=useState({});
+    const collectionRef=collection(database,'mail');
+    const collectionRefMsg=collection(database,'message')
+    const handleChange=(e)=>{
+        const {name,value}=e.target;
+        setMail({...mail,[name]:value})
+    }
+    // console.log(mail);
 
-  })
-
-  const handleChange=(e)=>{
-      console.log(e.target)
+    const handleChangeMsg=(e)=>{
       const {name,value}=e.target;
-      console.log(name,value)
-      setEmail({...email,[name]:value})
-  }
-  console.log(email)
+      setMsg({...msg,[name]:value});
+    }
+
+
+    const handleSubmitMail=(e)=>{
+      e.preventDefault();
+      // firebase ADD data
+      addDoc(collectionRef,{
+        name:mail.name,
+        email:mail.email,
+        message:mail.message
+      }).then(()=>{
+          alert('Msg send')
+      }).catch(()=>{
+          alert('Msg not send')
+      })
+    }
+
+    const handleSubmitMsg=(e)=>{
+      e.preventDefault();
+      // firebase ADD data
+      addDoc(collectionRefMsg,{
+        name:msg.name,
+        phone:msg.phone,
+        message:msg.message
+      }).then(()=>{
+          alert('Mail send')
+      }).catch(()=>{
+          alert('Mail not send')
+      })
+    }
   return (
     <div>
       <div className={style.ContactUsContainer}>
@@ -48,7 +83,7 @@ export const ContactUs = () => {
             {/* div.container */}
             <h2>Send us a Email</h2>
             <div className="form">
-              <form>
+              <form onSubmit={handleSubmitMail}>
                 <label htmlFor="name">Name:-</label>
                 <input type="text" id="name" name="name" required onChange={handleChange}/>
 
@@ -58,7 +93,7 @@ export const ContactUs = () => {
                 <label htmlFor="message">Message:</label>
                 <textarea id="message" name="message" rows="4" required onChange={handleChange}></textarea>
 
-                <button type="submit">Send Message</button>
+                <button type="submit">Send Mail</button>
               </form>
             </div>
             
@@ -67,15 +102,18 @@ export const ContactUs = () => {
             {/* div.container */}
             <h2>Send us a Message</h2>
             <div className="form">
-              <form>
+              <form onSubmit={handleSubmitMsg}>
                 <label htmlFor="name">Name:-</label>
-                <input type="text" id="name" name="name" required />
+                <input type="text" id="name" name="name" required onChange={handleChangeMsg}/>
 
-                <label htmlFor="email">Email:</label>
-                <input type="email" id="email" name="email" required />
+                {/* <label htmlFor="email">Email:</label>
+                <input type="email" id="email" name="email" required /> */}
+
+                <label for="phone">Phone Number:</label>
+                <input type="tel" id="phone" name="phone" pattern="[0-9]{10}" required placeholder="Enter a 10-digit phone number" onChange={handleChangeMsg}/>
 
                 <label htmlFor="message">Message:</label>
-                <textarea id="message" name="message" rows="4" required></textarea>
+                <textarea id="message" name="message" rows="4" required onChange={handleChangeMsg}></textarea>
 
                 <button type="submit">Send Message</button>
               </form>
